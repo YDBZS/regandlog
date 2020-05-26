73b915e36d2248a90c6516efd24e2961ae6fec01
Vicetone <y908063221@gmail.com>
2020-05-26 17:01:18 +0800

    JavaDoc优化

--- src/main/java/com/ydbzs/Service/Impl/LoginServiceImpl.java
+++ src/main/java/com/ydbzs/Service/Impl/LoginServiceImpl.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * @Auth：多宝
- * @Date：2020/4/22 10:56
- * @Project：regandlog com.ydbzs.Service.Impl
- * @Description：
+ * @author ：多宝
+ * @date ：2020/4/22 10:56
  */
 @Service
 public class LoginServiceImpl implements LoginService {
@@ -33,11 +31,11 @@ public class LoginServiceImpl implements LoginService {
 
 
     /**
-     * @Author：多宝
-     * @Date：2020/4/23 16:32
-     * @Params：[tel]
-     * @Reture：java.lang.String
-     * @Descrip：根据用户的一个账号查询用户在数据库存储的一个盐值
+     * 根据用户的一个账号查询用户在数据库存储的一个盐值
+     * @author ：多宝
+     * @date ：2020/4/23 16:32
+     * @params ：[tel]
+     * @return ：java.lang.String
      */
     @Override
     public String findSalt(String tel) {
@@ -46,11 +44,11 @@ public class LoginServiceImpl implements LoginService {
     }
 
     /**
-     * @Author：多宝
-     * @Date：2020/4/23 15:56
-     * @Params：[tel, pass]
-     * @Reture：com.ydbzs.pojo.User
-     * @Descrip：用户登录的方法
+     * 用户登录的方法
+     * @author ：多宝
+     * @date ：2020/4/23 15:56
+     * @params ：[tel, pass]
+     * @return ：com.ydbzs.pojo.User
      */
     @Override
     public User searchByTelAndPass(String tel, String Dbpass) {
